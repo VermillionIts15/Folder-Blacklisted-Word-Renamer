fa24cbce36f5b5f5658f0256b7be23195e89ab24
Michael K. Vermillion <95695154+VermillionIts15@users.noreply.github.com>
2023-09-10 21:15:00 -0300

    Update and rename script.py to renamer.py
    feat: Add Configuration System and Error Handling
    
    This commit introduces a configuration system that allows users to customize the blacklist, whitelist, and target folder path via a `config.json` file. The script gracefully handles errors and provides informative error messages. Here's an overview of the changes:
    
    - Configuration System:
      - Users can define the blacklist, whitelist, and target folder path in the `config.json` file.
      - The `IsFolderPath` toggle controls whether the script uses its own directory or the specified folder as the target.
      - The script checks for the existence of `config.json` and provides appropriate warnings if it's missing.
      
    - Error Handling:
      - The script includes robust error handling to catch exceptions and display error messages without revealing sensitive information.
      - Error messages provide details to help users diagnose and address issues.
    
    Usage instructions and an example `config.json` file are also provided in the README.md.
    
    This update improves user customization, error management, and overall script reliability.

--- renamer.py
+++ renamer.py
@@ -1,32 +1,27 @@
 import os
+import json
 import random
 import string
 import datetime
 
-IsFolderPath = True  # Set to True if you want to use the script's directory as the target folder
-FolderPath = "/path/to/your/directory" # Will NOT work if IsFolderPath is set to False.
-
-blacklist = {
-    "blacklisted_name1": "replacement1",
-    "blacklisted_name2": "replacement2",
-}
-
-whitelist = {
-    "file_to_keep1": "file_to_keep1",
-}
-
-if IsFolderPath:
-    directory = os.path.dirname(os.path.abspath(__file__))
-else:
-    directory = FolderPath
-
-log_directory = os.path.join(directory, "log")
-os.makedirs(log_directory, exist_ok=True)
+def load_config():
+    try:
+        with open("config.json", "r") as config_file:
+            config_data = json.load(config_file)
+        return config_data
+    except FileNotFoundError:
+        print("Error: config.json not found. Please create a config file.")
+        return None
+    except Exception as e:
+        print(f"Error: An unexpected error occurred while loading the config file. {str(e)}")
+        return None
 
 def generate_random_sequence():
     return ''.join(random.choices(string.digits, k=5))
 
 def rename_files(directory, blacklist, whitelist):
+    log_directory = os.path.join(directory, "log")
+    os.makedirs(log_directory, exist_ok=True)
     log_file_name = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S") + ".log"
     log_file_path = os.path.join(log_directory, log_file_name)
     with open(log_file_path, "w") as log_file:
@@ -51,13 +46,30 @@ def rename_files(directory, blacklist, whitelist):
                     else:
                         print(f"Skipped: {filename} (Whitelisted)")
 
-while True:
-    print("(1) Start Renaming")
-    print("(2) Exit")
-    choice = input("Enter your choice: ")
-    if choice == "1":
-        rename_files(directory, blacklist, whitelist)
-    elif choice == "2":
-        break
+def main():
+    config = load_config()
+    if config is None:
+        return
+
+    blacklist = config.get("blacklist", {})
+    whitelist = config.get("whitelist", {})
+    IsFolderPath = config.get("IsFolderPath", True)
+    
+    if not IsFolderPath:
+        directory = os.path.dirname(os.path.abspath(__file__))
     else:
-        print("Invalid choice. Please choose again.")
+        directory = config.get("folder_path", os.path.dirname(os.path.abspath(__file__)))
+
+    while True:
+        print("(1) Start Renaming")
+        print("(2) Exit")
+        choice = input("Enter your choice: ")
+        if choice == "1":
+            rename_files(directory, blacklist, whitelist)
+        elif choice == "2":
+            break
+        else:
+            print("Invalid choice. Please choose again.")
+
+if __name__ == "__main__":
+    main()
