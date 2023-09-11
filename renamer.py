ee91c47305e3e4cbaab8f6b1ed7cf1b9ff40d4d7
Michael K. Vermillion <95695154+VermillionIts15@users.noreply.github.com>
2023-09-10 22:04:13 -0300

    Update renamer.py
    backup

--- renamer.py
+++ renamer.py
@@ -3,6 +3,7 @@ import json
 import random
 import string
 import datetime
+import shutil
 
 def load_config():
     try:
@@ -12,6 +13,9 @@ def load_config():
     except FileNotFoundError:
         print("Error: config.json not found. Please create a config file.")
         return None
+    except json.JSONDecodeError as e:
+        print(f"Error: Unable to parse config.json. {str(e)}")
+        return None
     except Exception as e:
         print(f"Error: An unexpected error occurred while loading the config file. {str(e)}")
         return None
@@ -19,7 +23,20 @@ def load_config():
 def generate_random_sequence():
     return ''.join(random.choices(string.digits, k=5))
 
-def rename_files(directory, blacklist, whitelist):
+def create_backup(directory, backup_directory):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
+    backup_directory = os.path.join(backup_directory, f"backup_{timestamp}")
+    os.makedirs(backup_directory, exist_ok=True)
+    
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, os.path.join(backup_directory, filename))
+
+def rename_files(directory, blacklist, whitelist, create_backup=False):
+    if create_backup:
+        create_backup(directory, directory)
+    
     log_directory = os.path.join(directory, "log")
     os.makedirs(log_directory, exist_ok=True)
     log_file_name = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S") + ".log"
@@ -54,18 +71,24 @@ def main():
     blacklist = config.get("blacklist", {})
     whitelist = config.get("whitelist", {})
     IsFolderPath = config.get("IsFolderPath", True)
+    create_backup_flag = config.get("create_backup", False)  # New config option for creating backups
     
     if not IsFolderPath:
         directory = os.path.dirname(os.path.abspath(__file__))
     else:
         directory = config.get("folder_path", os.path.dirname(os.path.abspath(__file__)))
 
+    # Check if the specified target folder exists
+    if not os.path.exists(directory):
+        print(f"Error: Target folder '{directory}' does not exist.")
+        return
+
     while True:
         print("(1) Start Renaming")
         print("(2) Exit")
         choice = input("Enter your choice: ")
         if choice == "1":
-            rename_files(directory, blacklist, whitelist)
+            rename_files(directory, blacklist, whitelist, create_backup_flag)  # Pass create_backup_flag
         elif choice == "2":
             break
         else:
